ae5a684241ef1eccdb9184ba82ce34eeea25b2f5
adnan2508 <adnandiu2800@gmail.com>
2024-02-19 00:38:35 +0600

    getting interactive

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const allSeat = document.getElementsByClassName('seat-btn');
+console.log(allSeat);
+let seatCount = 0;
+let seatLeft = 16;
+let seatPrice = 0;
+
+for(const seat of allSeat) {
+    seat.addEventListener('click', function(event) {
+        seat.style.backgroundColor = '#1DD100';
+        seat.style.color = 'white';
+        seatCount = seatCount + 1;
+        seatLeft = seatLeft - 1;
+        seatPrice = seatCount * 550;
+        document.getElementById('seat-count').innerText = seatCount;
+        document.getElementById('seat-left').innerText = seatLeft;
+        document.getElementById('seat-price').innerText = seatPrice;
+    })
+}
